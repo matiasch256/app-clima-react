ff0000208012dd1dbdbd0536c6a76ce29a48b443
Matias <matiasch256@gmail.com>
2025-03-17 19:33:50 -0300

    Actualizacion

--- src/AppClima.jsx
+++ src/AppClima.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./AppClima.css";
+import { NavBar } from "./components/navbar/NavBar";
 
 export const AppClima = () => {
   const urlBase = "https://api.openweathermap.org/data/2.5/weather";
@@ -29,38 +30,47 @@ export const AppClima = () => {
   };
 
   return (
-    <div className="container">
-      <h1 className="title">Clima App</h1>
-      <form onSubmit={handleSubmit} className="weather-form">
-        <input
-          type="text"
-          placeholder="Ingresa una ciudad..."
-          value={city}
-          onChange={handleCityChange}
-        />
-        <button type="submit">Buscar</button>
-      </form>
+    <>
+      <NavBar />
+      <div className="container">
+        <form onSubmit={handleSubmit} className="weather-form">
+          <input
+            type="text"
+            placeholder="Ingresa una ciudad..."
+            value={city}
+            onChange={handleCityChange}
+          />
+          <button type="submit">Buscar</button>
+        </form>
 
-      <div className="weather-info">
-        {weather ? (
-          <>
-            <h2>
-              Ciudad: {weather.name}, País:
-              {weather.sys.country === "AR" ? "Argentina" : weather.sys.country}
-            </h2>
-            <p>Temperatura: {Math.floor(weather.main.temp - difKelvin)}°C</p>
-            <p>
-              Condición meteorologica actual: {weather.weather[0].description}{" "}
-            </p>
-            <img
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt=""
-            />
-          </>
-        ) : (
-          <p>Ingresa una ciudad y presiona buscar.</p>
-        )}
+        <div className="weather-info">
+          {weather ? (
+            <>
+              <h2>
+                Ciudad: {weather.name}, País:
+                {weather.sys.country === "AR"
+                  ? "Argentina"
+                  : weather.sys.country}
+              </h2>
+              <p>Temperatura: {Math.floor(weather.main.temp - difKelvin)}°C</p>
+              <p>Humedad: {weather.main.humidity}%</p>
+              <p>Visibilidad: {weather.visibility / 1000} KM</p>
+              <p>Velocidad del viento: {Math.floor(weather.wind.speed)} km/h</p>
+              <p>Velocidad del viento: {Math.floor(weather.wind.speed)} km/h</p>
+              <p>Presion Atmosferica: {weather.main.pressure} KPa</p>
+              <p>
+                Condición meteorologica actual: {weather.weather[0].description}{" "}
+              </p>
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt=""
+              />
+            </>
+          ) : (
+            <p className="pInfo">Ingresa una ciudad y presiona buscar.</p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+export const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className=" mx-auto navbar-title">CLIMA</a>
+        </div>
+      </nav>
+      ;
+    </>
+  );
+};
